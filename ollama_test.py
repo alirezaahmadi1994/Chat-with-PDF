228141024951e3d5f8b0fabe5d0cc81d2fe8a6d7
Alireza Ahmadi <alireza.ahmadi@vetted.work>
2025-05-25 12:40:55 +0330

    Remove ollama_test.py

--- ollama_test.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-client = ollama.Client(host='http://127.0.0.1:11434')
-
-response = client.chat(model='mistral', messages=[
-    {'role': 'user', 'content': 'What is the capital of France?'},
-])
-print(response['message']['content'])
